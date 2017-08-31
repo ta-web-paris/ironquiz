94ac89dfbdb82da5256b7d46c706c5e02bea4434
Yacine Hmito <yacine.hmito@gmail.com>
2017-08-31 09:57:35 +0200

    Cleanup Nuxt scaffolding
    - Removed README.md in the subfolders and put their content in the root README.md
    - Removed original components
    - Updated Nuxt to 1.0.0-rc8
    - Disable SSR

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,15 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
+  mode: 'spa',
   head: {
     title: 'ironquiz',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Realtime quiz application for bootcamps' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Customize the progress-bar color
-  */
   loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
   build: {
-    /*
-    ** Run ESLINT on save
-    */
     extend (config, ctx) {
       if (ctx.dev && ctx.isClient) {
         config.module.rules.push({
