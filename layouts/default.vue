19bf1bf17a77360a17abaa48ecb3b7112df16e2e
Yacine Hmito <yacine.hmito@gmail.com>
2017-09-03 13:01:40 +0200

    Setting up Bulma

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,16 @@
     <nuxt/>
   </div>
 </template>
+
+<style lang="scss">
+@import "~bulma/sass/utilities/initial-variables";
+
+// Ironhack's blue from the website's logo
+$blue: rgb(40, 175, 250);
+$blue-invert: rgb(255, 255, 255);
+
+$primary: $blue;
+$primary-invert: $blue-invert;
+
+@import "~bulma/bulma.sass";
+</style>
